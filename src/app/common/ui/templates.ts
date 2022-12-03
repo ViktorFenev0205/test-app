d07257b23d002ce3cbefe3a360a08548a669b611
Viktor Fenev <victor.fenev.exceed@gmail.com>
2022-12-03 11:45:40 +0300

    Fix english text

--- src/app/common/ui/templates.ts
+++ src/app/common/ui/templates.ts
@@ -2,7 +2,7 @@
 //Right now do not see necessary to move style to less file. Just for test
 export const IMAGE_TOOLTIP = `
   <div style="display: flex; flex-direction: column; align-items: center; justify-content: center">
-    <div style="color: aqua; text-align: center">Test cat. My cat! Look straight into the soul</div>
+    <div style="color: aqua; text-align: center">Test cat. My cat! Looks right into your soul</div>
     <img src="https://drive.google.com/uc?export=view&id=1qKhmb2OTZ3FPpDGMfMOxR96iTIfpEAGb" width="250" height="250" alt="">
   </div>
 `;
